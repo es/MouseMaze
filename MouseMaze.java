682232674bb7e50c598cb093a0f58c0035b3d11c
Emil Stolarsky <emil.stolarsky@gmail.com>
2013-03-10 00:45:27 -0500

    Started working on maze object

--- MouseMaze.java
+++ MouseMaze.java
@@ -4,41 +4,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
 
-public class MouseMaze extends JFrame implements ActionListener {
+public class MouseMaze {
 	
 	public MouseMaze () {
-		super ();
-		this.setTitle ("Mouse Traversing Maze Simulator");
-		this.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		this.setSize (500,500);
-		this.setResizable(false);
-		BorderLayout layout = new BorderLayout (0,0);
-		this.setLayout (layout);
-
-		JMenuBar menuBar= new JMenuBar ();
-		JMenu file = new JMenu ("File");
-
-		JMenuItem newMaze = new JMenuItem ("New Maze");
-		newMaze.setActionCommand ("newMaze");
-		newMaze.addActionListener(this);
-		file.add(newMaze);
-
-		JMenuItem exit = new JMenuItem ("Exit");
-		exit.setActionCommand("exit");
-		exit.addActionListener(this);
-		file.add(exit);
-
-		menuBar.add (file);
-		this.setJMenuBar (menuBar);
-
-		this.setVisible (true);
+		MainWindow window = new MainWindow ();
 	}
-
-	public void actionPerformed(ActionEvent ae) {
-        if (ae.getActionCommand().equals("exit")) {
-          	System.exit (0);
-        }
-    }
 
 	public static void main (String [] args) {
 		new MouseMaze();
