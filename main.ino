94e6b5d5de1e6d02cdef9d97452a7deb3a30ef7b
Matthew Levy <matthewlevy24@gmail.com>
2013-04-23 19:27:09 -0300

    Pin map and LDR value->boolean conversion
    Mapped out the pins that are currently in use in top comments
    Made an array of booleans that store if each LDR is above black
    Made a method that stores the boolean values

--- main.ino
+++ main.ino
@@ -1,10 +1,35 @@
+/* Pin Map:
+Digital:
+0 - Avoid if possible. Serial output
+1 - Avoid if possible. Serial output
+2 - Red LED
+3 - Yellow LED
+4 - Green LED
+5 - Calibration button
+6 - Motor 1A
+7 - Motor 1B
+8 - Motor 2A
+9 - Motor 2B
+10 - Bright LED? Gets turned off if Mouse is using Ultrasonic instead
+11 - Empty
+12 - Empty
+13 - Empty
+Anlog:
+A0 - LDR or Ultrasonic
+A1 - LDR or Ultrasonic
+A2 - LDR
+A3 - LDR
+A4 - LDR
+A5 - Empty
+
 int redLED = 2; //LED pin indicating: Press button to set white values
 int yellowLED = 3; //LED pin indicating: Press button to set black values
 int greenLED = 4; //LED pin indicating: Press button to go
-int button = 5; //Digital input for button
+int button = 5; //Digital input for calibration button
 int[5] white; //Value saved on white button press
 int[5] black; //Value saved on black button press
 int[5] threshold; //Average between white and black.
+boolean[5] ldr; //1 if over black, 0 otherwise
 
 void setup(){
   pinMode(redLED, OUTPUT);
@@ -39,5 +64,14 @@ void readBlack(){
 void setThresholds(){
   for (int i = 0; i<5; i++){
     threshold[i] = (white[i]+black[i])/2;
+  }
+}
+
+void setLDRValues(){
+  for (int i = 0; i<5; i++){
+    if (analogRead(i)<threshold[i])
+      ldr[i]=0;
+    else
+      ldr[i]=1;
   }
 }
